a22f38629a19ebe1ca61ebc5d765fd6d58062080
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2025-05-24 20:32:34 +1000

    feat: Add release-please for automated releases

--- transcribe_me/__init__.py
+++ transcribe_me/__init__.py
@@ -1,7 +1,3 @@
-import os
+"""Transcribe Me - A CLI tool to transcribe audio files."""
 
-__version__ = os.environ.get("VERSION", "0.0.0")
-
-parts = __version__.split(".")
-if len(parts) != 3 or not all(part.isdigit() for part in parts):
-    __version__ = "0.0.0"
+__version__ = "0.1.0"
